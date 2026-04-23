77e7c0d225fa2b72d771e4e40e2d14b885e1895a
quwaila82-commits <quwaila82@gmail.com>
2026-04-23 15:56:51 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+var CACHE = "sambu-net-v6";
+var URLS = ["/"];
+self.addEventListener("install", function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(URLS); }));
+  self.skipWaiting();
+});
+self.addEventListener("activate", function(e) {
+  e.waitUntil(caches.keys().then(function(names) {
+    return Promise.all(names.filter(function(n) { return n !== CACHE; }).map(function(n) { return caches.delete(n); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener("fetch", function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+});
