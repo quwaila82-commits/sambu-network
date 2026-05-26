f37220dd0a5695d948fecfb6475cf241a492a64d
quwaila82-commits <quwaila82@gmail.com>
2026-05-26 15:06:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "sambu-net-v11";
+var CACHE = "sambu-net-v13";
 var URLS = ["/"];
 self.addEventListener("install", function(e) {
   e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(URLS); }));
